01add7fb63e9f751c46e1c4beb35bf723bb62726
AfanasevYN <Kwice2@yandex.ru>
2024-08-28 20:46:46 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,10 @@
+immutable_var = ("топор", 5, "артерия", 2, "календарь", 3, "спорт", True)
+print(immutable_var)
+#immutable_var[0] = "нож"
+#TypeError: 'tuple' object does not support item assignment
+mutable_list = ["топор", 5, "артерия", 2, "календарь", 3, "спорт", 1]
+print(mutable_list)
+mutable_list.append("ага")
+print(mutable_list)
+
+
