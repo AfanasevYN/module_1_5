a27ccb272b6703b83c6ca9c154e892618fd53fca
AfanasevYN <Kwice2@yandex.ru>
2024-08-31 19:11:13 +0300

    Update module_1_5.py

--- module_1_5.py
+++ module_1_5.py
@@ -4,7 +4,7 @@ print(immutable_var)
 #TypeError: 'tuple' object does not support item assignment
 mutable_list = ["топор", 5, "артерия", 2, "календарь", 3, "спорт", 1]
 print(mutable_list)
-mutable_list.append("ага")
+mutable_list[7] = 'понял'
 print(mutable_list)
 
 
